7f5eccd7358d675441be243350359802576585e9
kendavidson <ken.j.davidson@live.ca>
2022-09-15 01:37:18 +0000

    feat(api): added api project

---
+++ maven-release-test-api/src/main/java/kjd/playground/PersonApi.java
@@ -0,0 +1,14 @@
+package kjd.playground;
+
+import java.util.Collections;
+import java.util.List;
+
+public class PersonApi {
+    public Person get(Long id) {
+        return null;
+    }
+
+    public List<Person> all() {
+        return Collections.emptyList();
+    }
+}
