bb1bc5e66cc64e443858344f15eada46284aab01
kendavidson <ken.j.davidson@live.ca>
2022-09-15 00:44:02 +0000

    chore(person): updated tests for person

--- maven-release-test-data/src/test/java/kjd/playground/PersonTest.java
+++ maven-release-test-data/src/test/java/kjd/playground/PersonTest.java
@@ -1,5 +1,9 @@
 package kjd.playground;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class PersonTest {
 
     @Test
@@ -9,6 +13,16 @@ public class PersonTest {
         assertEquals("Ken", person.GivenName);
         assertEquals("Davidson", person.Surname);
         assertEquals(42, person.Age);
+    }
+
+    @Test
+    public void testNewPersonFromPerson() {
+        Person person = new Person("Ken", "Davidson", 42);
+        Person person2 = new Person(person);
+
+        assertEquals("Ken", person2.GivenName);
+        assertEquals("Davidson", person2.Surname);
+        assertEquals(42, person2.Age);
     }
     
 }
